e263fa764bae48afa24cbd802e34aea1c54e1faa
berlinHsin <berlin.hsin@gmail>
2015-02-03 18:47:20 +0800

    UI funct

--- app/assets/javascripts/mobile.js
+++ app/assets/javascripts/mobile.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
+    $('[hit=second]').hide();
+    $('[hit=third]').hide();
 	$('#bottom-nav li').click(function(){
 		$('#bottom-nav li').attr('class','');
+        $hit = $(this).attr('target');
 		$(this).attr('class','active');
+        $('[hit]').hide();
+        $('[hit='+$hit+']').show();
 	});
 });
